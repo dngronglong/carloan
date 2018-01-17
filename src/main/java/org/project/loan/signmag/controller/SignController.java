cb0f6dd1eef3b3a391bd6a0521b7651a9f1523d9
dongronglong <24197396@qq.com>
2018-01-17 14:45:05 +0800

    添加了注释

--- src/main/java/org/project/loan/signmag/controller/SignController.java
+++ src/main/java/org/project/loan/signmag/controller/SignController.java
@@ -16,7 +16,7 @@ public class SignController {
     private ISignWriteService iSignWriteService;
 
     /**
-     * 添加签约信息
+     * 添加签约信息，添加了注释
      *
      */
     @RequestMapping(value = "/sign/addSign",method = RequestMethod.POST)
